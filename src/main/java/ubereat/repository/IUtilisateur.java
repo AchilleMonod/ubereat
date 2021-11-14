015188a046ec37378a85d4953351f2270e89eda8
aymeric <aymeric-dehorter@hotmail.fr>
2021-11-14 10:59:00 +0100

    Interface

--- src/main/java/ubereat/repository/IUtilisateur.java
+++ src/main/java/ubereat/repository/IUtilisateur.java
@@ -1,5 +1,11 @@
 package ubereat.repository;
 
-public class IUtilisateur {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ubereat.model.Utilisateur;
+
+
+public interface IUtilisateur extends JpaRepository<Utilisateur, Long> {
+
 
 }
